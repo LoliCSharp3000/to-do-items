178fa918e04a9daed9ca4d75e0dc7e5afc42df02
LoliCSharp3000 <araara3090ti@gmail.com>
2025-10-06 10:07:34 +0200

    En produccion

---
+++ gestiondetareas/src/main/java/microservicios/gestiondetareas/GestiondetareasApplication.java
@@ -0,0 +1,11 @@
+package microservicios.gestiondetareas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestiondetareasApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GestiondetareasApplication.class, args);
+    }
+}
